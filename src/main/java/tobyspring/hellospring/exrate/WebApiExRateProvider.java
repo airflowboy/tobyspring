e6fe9f5bff33d7d51f4f8b0cfdd7e8c90b8a46a5
airflowboy <ajtwoddl9194@gmail.com>
2024-12-02 20:29:14 +0900

    토비의 스프링 템플릿

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,53 +1,24 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import org.springframework.stereotype.Component;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.ApiTemplate;
+import tobyspring.hellospring.api.ErApiExRateExtractor;
+import tobyspring.hellospring.api.HttpClientApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
 @Component
 public class WebApiExRateProvider implements ExRateProvider {
 
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
+
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url);
-    }
-
-    private static BigDecimal runApiForExRate(String url) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = new SimpleApiExecutor().execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return extractExRate(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static BigDecimal extractExRate(String response) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
 }
